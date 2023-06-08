bee6b8295a7bd94c083c220563778366e42c308e
Jakub Solawa <jkbsolawa@gmail.com>
2023-06-08 16:53:35 +0200

    Initial commit

---
+++ src/main/java/com/zerkinisoft/peatbit/PeatBitApplication.java
@@ -0,0 +1,13 @@
+package com.zerkinisoft.peatbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeatBitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeatBitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerkinisoft/peatbit/PeatBitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerkinisoft.peatbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeatBitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
