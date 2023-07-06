27f0f4bf5921430abc4541014ee568d0f408a178
Jakub Solawa <jkbsolawa@gmail.com>
2023-07-06 14:41:21 +0200

    Basic endpoints with persistence

--- src/main/java/com/zerkinisoft/peatbit/domain/LiquorService.java
+++ src/main/java/com/zerkinisoft/peatbit/domain/LiquorService.java
@@ -1,5 +1,7 @@
 package com.zerkinisoft.peatbit.domain;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
 
 @Service
